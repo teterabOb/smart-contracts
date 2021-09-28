d2f1d886621614b6397f63bfefa3b3b7773b40e0
Gilberts Ahumada <gilbertsahumada@gmail.com>
2021-09-27 22:19:50 -0300

    Create MyFirstContract.sol

---
+++ MyFirstContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.7;
+
+contract MyFirstContract{
+    uint256 number;
+    
+    function changeNumber(uint256 _num) public {
+        number = _num;
+    }
+    
+    function getNumber() public view returns(uint256){
+        return number;
+    }
+}
