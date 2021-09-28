6239e3faf7320e4a986888902f5605889a7d4417
Gilberts Ahumada <gilbertsahumada@gmail.com>
2021-09-27 22:22:25 -0300

    Update Mappings.sol

--- Mappings.sol
+++ Mappings.sol
@@ -1,1 +1,23 @@
 
+pragma solidity ^0.6.7;
+
+contract Mappings{
+    string[] names; 
+    mapping (string => uint) public phoneNumbers;
+    
+    function addMobilePhoneNumber(string memory _name, uint _mobileNumber) public{
+        phoneNumbers[_name] = _mobileNumber;
+    }
+    
+    function getMobileNumber(string memory _name) public view returns(uint){
+        return phoneNumbers[_name];
+    }
+    
+    function changeName(string memory _name) public {
+        names.push(_name);
+    }
+    
+    function getName(uint256 _num) public view returns(string memory){
+        return names[_num];
+    }
+}
